f9c880a1dfaec77642ab02e0da6c657438744cc9
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2023-02-05 09:07:35 -0600

    $emit
    A new stage that emits messages to a stream specified in the connection registry.

---
+++ Emit.js
@@ -0,0 +1,10 @@
+// kafka
+
+{
+   "$emit": {
+	"name": "<connection from connection registry>",
+	"topic" : "<topic from that connection to produce to>",
+      "config": { <document of producer properties> }
+    }
+}
+
